d779bb8774c22fb8dc242ed2f3ffc5fa82dfa46a
fifi000 <filipkociok@gmail.com>
2022-05-29 19:03:04 +0200

    Wire Up Login Form to API
    Connects the login form button to the  authentication API endpoint (/token). Gets back the bearer token or an exeption if failed.

---
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,74 @@
+﻿using Caliburn.Micro;
+using RMDesktopUI.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RMDesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _userName;
+        private string _userPassword;
+        private IAPIHelper _apiHelper;
+
+        public string UserName 
+        {
+            get { return _userName; }
+            set 
+            { 
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+        public string UserPassword 
+        {
+            get { return _userPassword; }
+            set 
+            {
+                _userPassword = value;
+                NotifyOfPropertyChange(() => UserPassword);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public LoginViewModel(IAPIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        public bool CanLogIn
+        {
+            get 
+            {
+                bool output = false;
+
+                if (String.IsNullOrEmpty(UserName) == false && String.IsNullOrEmpty(UserPassword) == false)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+            
+        }
+
+        public async Task LogIn() 
+        {
+            try
+            {
+                var result = await _apiHelper.Authenticate(UserName, UserPassword);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
